4ff5d650055c25e142677e6409ad0abe5efd333f
celine kaslin <celine.kaslin@gmail.com>
2018-12-07 15:05:02 +0000

    step14 start method in journey class

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -4,4 +4,11 @@ describe Journey do
   it "should return true when we start journey" do
     expect(subject).to respond_to :status
   end
+
+  it "should return true after journey start" do
+    expect(subject.start).to eq true
+  end
+
+
+
 end
