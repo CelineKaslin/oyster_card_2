79012e14ff1b11380dd4b05261fa1f6cdd01e279
celine kaslin <celine.kaslin@gmail.com>
2018-12-07 16:45:49 +0000

    step15 exit method in history class

--- spec/history_spec.rb
+++ spec/history_spec.rb
@@ -1,8 +1,17 @@
 require "history"
 describe History do
+
   let(:status) {double :status}
   let(:entry_station) {double :entry_station}
-  it "will return a entry station" do
+  let(:exit_station) {double :exit_station}
+  
+  it "should return the entry station" do
     expect(subject.entry(:entry_station)).to eq :entry_station
   end
+
+  it "should return the exit station" do
+    expect(subject.exit(:exit_station)).to eq :exit_station
+  end
+
+
 end
