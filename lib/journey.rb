26328a1d5cefc7bf65bc1df43a3d09735010ae7e
Hui An Yang <hecatie36@hotmail.com>
2018-12-07 15:46:48 +0000

    step14 method fare added in journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,6 +1,8 @@
 class Journey
   attr_reader :status
-
+  MIN_FARE = 1
+  PENALTY_FARE = 6
+  
   def initialize
     @status = nil
     @entry = nil
@@ -15,6 +17,10 @@ class Journey
   def finish(exit)
     @exit = exit
     @status = false
+  end
+
+  def fare
+    in_use? ? PENALTY_FARE : MIN_FARE
   end
 
   def in_use?
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -22,6 +22,17 @@ describe Journey do
     subject.start(:entry)
     expect(subject.in_use?).to eq true
   end
+  context "#fare" do
+    it "should return min fare" do
+      subject.start(:entry)
+      subject.finish(:exit)
+      expect(subject.fare).to eq Journey::MIN_FARE
+    end
+    it "should return penalty fare" do
+      subject.start(:entry)
+      expect(subject.fare).to eq Journey::PENALTY_FARE
+    end
+  end
 
 
 end
